3fc24cd595e86beec0ff7640318c22748849ae88
bostoncleek <boston@picknik.ai>
2021-01-13 16:48:11 -0700

    IK working in rviz

--- launch/move_arm.py
+++ launch/move_arm.py
@@ -1,31 +1,32 @@
 import os
+import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from ament_index_python.packages import get_package_share_directory
 
+# def load_file(package_name, file_path):
+#     package_path = get_package_share_directory(package_name)
+#     absolute_file_path = os.path.join(package_path, file_path)
+
+#     try:
+#         with open(absolute_file_path, 'r') as file:
+#             return file.read()
+#     except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+#         return None
+
 def generate_launch_description():
 
-    # robot_urdf_file = os.path.join(get_package_share_directory("simple_arm"), "urdf", "arm.urdf")
-    robot_urdf_file = os.path.join(get_package_share_directory("moveit_resources_panda_description"), "urdf", "panda.urdf")
-    print("urdf_file_name : {}".format(robot_urdf_file))
+    robot_description_config = xacro.process_file(os.path.join(get_package_share_directory('simple_arm'), 'urdf', 'arm.urdf.xacro'))
 
-    robot_description = {'robot_description' : robot_urdf_file}
-
+    robot_description = {'robot_description' : robot_description_config.toxml()}
 
     rviz_node = Node(package='rviz2',
                      executable='rviz2',
                      name='rviz2',
                      output='log',
-                     arguments=['-d', os.path.join(get_package_share_directory("simple_arm"), "config", "demo.rviz")],
-                     parameters=[robot_description])
-
-    # robot_state_publisher = Node(package='robot_state_publisher',
-    #                              executable='robot_state_publisher',
-    #                              name='robot_state_publisher',
-    #                              output='screen',
-    #                              parameters=[{'robot_description':Command(['xacro',' ', robot_urdf_file]) }] ) 
+                     arguments=['-d', os.path.join(get_package_share_directory("simple_arm"), "config", "demo.rviz")])
 
     container = ComposableNodeContainer(
         name='arm_demo_container',
@@ -37,25 +38,20 @@ def generate_launch_description():
                 package='robot_state_publisher',
                 plugin='robot_state_publisher::RobotStatePublisher',
                 name='robot_state_publisher',
-                parameters=[robot_description])
+                parameters=[robot_description]),
+            ComposableNode(
+                package='simple_arm',
+                plugin='simple_arm::SimpleArm',
+                name='simple_arm',
+                parameters=[os.path.join(get_package_share_directory("simple_arm"), "config", "arm.yaml")])
         ], 
         output='screen',
     )
 
+    # jsp_node = Node(package='joint_state_publisher_gui',
+    #                 executable='joint_state_publisher_gui',
+    #                 name='joint_state_publisher_gui',
+    #                 parameters=[robot_description])
 
-    # container = ComposableNodeContainer(
-    #     name='arm_demo_container',
-    #     namespace='/',
-    #     package='rclcpp_components',
-    #     executable='component_container',
-    #     composable_node_descriptions=[
-    #         ComposableNode(
-    #             package='simple_arm',
-    #             plugin='simple_arm::SimpleArm',
-    #             name='simple_arm',
-    #             parameters=[os.path.join(get_package_share_directory("simple_arm"), "config", "arm.yaml")])
-    #     ], 
-    #     output='screen',
-    # )
 
     return LaunchDescription([ container, rviz_node ])
--- src/arm_node.cpp
+++ src/arm_node.cpp
@@ -4,6 +4,7 @@
  * @brief 2R arm 
 */
 // PUBLISHES: joint_states (JointState) - theta1 and theta2 corresponding to the two joint angles
+// IK derivation: https://robotacademy.net.au/lesson/inverse-kinematics-for-a-2-joint-robot-arm-using-geometry/
 
 #define _USE_MATH_DEFINES
 #include <chrono>                               
@@ -16,9 +17,8 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include "visualization_msgs/msg/marker.hpp"
-
-#include "simple_arm/msg/joint_state.hpp"
 
 using namespace std::chrono_literals;
 
@@ -36,13 +36,13 @@ public:
      , t_(0.0)
      , dt_(0.01)
     {
-        joint_pub_ = this->create_publisher<simple_arm::msg::JointState>("joint_states", 1);
-        marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("eef_marker", 1);
-        timer_ = this->create_wall_timer(20ms, std::bind(&SimpleArm::jointCallback, this));
+        joint_pub_ = create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        marker_pub_ = create_publisher<visualization_msgs::msg::Marker>("eef_marker", 10);
+        timer_ = create_wall_timer(10ms, std::bind(&SimpleArm::jointCallback, this));
 
-        l1_ = this->declare_parameter<double>("L1", 1.0);
-        l2_ = this->declare_parameter<double>("L2", 1.0);
-        period_ = this->declare_parameter<double>("T", 10.0);
+        l1_ = declare_parameter<double>("L1", 1.0);
+        l2_ = declare_parameter<double>("L2", 1.0);
+        period_ = declare_parameter<double>("T", 10.0);
 
         RCLCPP_INFO_STREAM(LOGGER, l1_);
         RCLCPP_INFO_STREAM(LOGGER, l2_);
@@ -56,34 +56,36 @@ private:
         const auto eef_position = endEffector();
         const auto joints = jointAngles(eef_position.at(0), eef_position.at(1));
 
-        simple_arm::msg::JointState joint_msg;
-        joint_msg.j1 = joints.at(0);
-        joint_msg.j2 = joints.at(1);
+        sensor_msgs::msg::JointState joint_msg;
+        joint_msg.header.stamp = now();
+        joint_msg.name = {"joint1", "joint2", "end"};
+        joint_msg.position = {joints.at(0), joints.at(1), 0.0};
         joint_pub_->publish(joint_msg);
 
-        // visualization_msgs::msg::Marker marker;
-        // marker.header.frame_id = "END_EFFECTOR";
-        // marker.action = visualization_msgs::msg::Marker::ADD;
-        // marker.pose.orientation.w = 1.0;
-        // marker.scale.x = 0.25;
-        // marker.scale.y = 0.25;
-        // marker.scale.z = 0.25;
-        // marker.lifetime = rclcpp::Duration(period_/5.0);
+        visualization_msgs::msg::Marker marker;
+        marker.header.frame_id = "End_Effector";
+        marker.header.stamp = now();
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 0.25;
+        marker.scale.y = 0.25;
+        marker.scale.z = 0.25;
+        marker.lifetime = rclcpp::Duration(period_/10.0);
 
-        // if (eef_position.at(0) > 0.0)
-        // {
-        //     marker.type = visualization_msgs::msg::Marker::CUBE;
-        //     marker.color.b = 1.0;
-        //     marker.color.a = 1.0;
-        // }
-        // else 
-        // {
-        //     marker.type = visualization_msgs::msg::Marker::ARROW;
-        //     marker.color.r = 1.0;
-        //     marker.color.a = 1.0;  
-        // }
+        if (eef_position.at(0) > 0.0)
+        {
+            marker.type = visualization_msgs::msg::Marker::CUBE;
+            marker.color.b = 1.0;
+            marker.color.a = 1.0;
+        }
+        else 
+        {
+            marker.type = visualization_msgs::msg::Marker::ARROW;
+            marker.color.r = 1.0;
+            marker.color.a = 1.0;  
+        }
 
-        // marker_pub_->publish(marker);
+        marker_pub_->publish(marker);
         
         // RCLCPP_INFO_STREAM(LOGGER, t_);
         // std::cout << "EEF(x,y): " << eef_position.at(0) << " " << eef_position.at(1) << std::endl;
@@ -115,7 +117,7 @@ private:
 
 private:
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<simple_arm::msg::JointState>::SharedPtr joint_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_pub_;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 
     double l1_, l2_, period_, h_, t_, dt_;
