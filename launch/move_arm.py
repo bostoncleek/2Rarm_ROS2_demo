41ac0ba0670080e0139fcc6fbe0232dfaee8b31a
bostoncleek <boston@picknik.ai>
2021-01-13 15:29:48 -0700

    robot urdf

--- launch/move_arm.py
+++ launch/move_arm.py
@@ -6,6 +6,27 @@ from launch_ros.descriptions import ComposableNode
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    # robot_urdf_file = os.path.join(get_package_share_directory("simple_arm"), "urdf", "arm.urdf")
+    robot_urdf_file = os.path.join(get_package_share_directory("moveit_resources_panda_description"), "urdf", "panda.urdf")
+    print("urdf_file_name : {}".format(robot_urdf_file))
+
+    robot_description = {'robot_description' : robot_urdf_file}
+
+
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     name='rviz2',
+                     output='log',
+                     arguments=['-d', os.path.join(get_package_share_directory("simple_arm"), "config", "demo.rviz")],
+                     parameters=[robot_description])
+
+    # robot_state_publisher = Node(package='robot_state_publisher',
+    #                              executable='robot_state_publisher',
+    #                              name='robot_state_publisher',
+    #                              output='screen',
+    #                              parameters=[{'robot_description':Command(['xacro',' ', robot_urdf_file]) }] ) 
+
     container = ComposableNodeContainer(
         name='arm_demo_container',
         namespace='/',
@@ -13,13 +34,28 @@ def generate_launch_description():
         executable='component_container',
         composable_node_descriptions=[
             ComposableNode(
-                package='simple_arm',
-                plugin='simple_arm::SimpleArm',
-                name='simple_arm',
-                parameters=[os.path.join(get_package_share_directory("simple_arm"), "config", "arm.yaml")])
+                package='robot_state_publisher',
+                plugin='robot_state_publisher::RobotStatePublisher',
+                name='robot_state_publisher',
+                parameters=[robot_description])
         ], 
         output='screen',
     )
 
-    return LaunchDescription([ container ])
-    
+
+    # container = ComposableNodeContainer(
+    #     name='arm_demo_container',
+    #     namespace='/',
+    #     package='rclcpp_components',
+    #     executable='component_container',
+    #     composable_node_descriptions=[
+    #         ComposableNode(
+    #             package='simple_arm',
+    #             plugin='simple_arm::SimpleArm',
+    #             name='simple_arm',
+    #             parameters=[os.path.join(get_package_share_directory("simple_arm"), "config", "arm.yaml")])
+    #     ], 
+    #     output='screen',
+    # )
+
+    return LaunchDescription([ container, rviz_node ])
--- src/arm_node.cpp
+++ src/arm_node.cpp
@@ -12,6 +12,7 @@
 #include <memory>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
@@ -37,7 +38,7 @@ public:
     {
         joint_pub_ = this->create_publisher<simple_arm::msg::JointState>("joint_states", 1);
         marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("eef_marker", 1);
-        timer_ = this->create_wall_timer(100ms, std::bind(&SimpleArm::jointCallback, this));
+        timer_ = this->create_wall_timer(20ms, std::bind(&SimpleArm::jointCallback, this));
 
         l1_ = this->declare_parameter<double>("L1", 1.0);
         l2_ = this->declare_parameter<double>("L2", 1.0);
@@ -60,27 +61,33 @@ private:
         joint_msg.j2 = joints.at(1);
         joint_pub_->publish(joint_msg);
 
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = "END_EFFECTOR";
-        marker.action = visualization_msgs::msg::Marker::ADD;
-        marker.pose.orientation.w = 1.0;
-        marker.scale.x = 0.25;
-        marker.scale.y = 0.25;
-        marker.scale.z = 0.25;
-        marker.lifetime = rclcpp::Duration(period_/5.0);
+        // visualization_msgs::msg::Marker marker;
+        // marker.header.frame_id = "END_EFFECTOR";
+        // marker.action = visualization_msgs::msg::Marker::ADD;
+        // marker.pose.orientation.w = 1.0;
+        // marker.scale.x = 0.25;
+        // marker.scale.y = 0.25;
+        // marker.scale.z = 0.25;
+        // marker.lifetime = rclcpp::Duration(period_/5.0);
 
-        if (eef_position.at(0) > 0.0)
-        {
-            marker.type = visualization_msgs::msg::Marker::CUBE;
-            marker.color.b = 1.0;
-            marker.color.a = 1.0;
-        }
-        else 
-        {
-            marker.type = visualization_msgs::msg::Marker::ARROW;
-            marker.color.r = 1.0;
-            marker.color.a = 1.0;  
-        }
+        // if (eef_position.at(0) > 0.0)
+        // {
+        //     marker.type = visualization_msgs::msg::Marker::CUBE;
+        //     marker.color.b = 1.0;
+        //     marker.color.a = 1.0;
+        // }
+        // else 
+        // {
+        //     marker.type = visualization_msgs::msg::Marker::ARROW;
+        //     marker.color.r = 1.0;
+        //     marker.color.a = 1.0;  
+        // }
+
+        // marker_pub_->publish(marker);
+        
+        // RCLCPP_INFO_STREAM(LOGGER, t_);
+        // std::cout << "EEF(x,y): " << eef_position.at(0) << " " << eef_position.at(1) << std::endl;
+        // std::cout << "Joints(j1,j2): " << joints.at(0) << " " << joints.at(1) << std::endl;
 
         t_ += dt_;
 
@@ -92,15 +99,17 @@ private:
 
     std::vector<double> endEffector()
     {
-        const auto x = 0.9 * std::cos(2.0 * M_PI * t_ / period_) * std::sqrt(std::pow(l1_ + l2_, 2) - std::pow(h_,2));
+        const auto x = 0.9 * std::cos(2.0 * M_PI * t_ / period_) * std::sqrt(std::pow(l1_ + l2_, 2) - h_*h_);
         const auto y = 2.0/3.0 * (l1_ + l2_);
         return { x, y };
     }
 
     std::vector<double> jointAngles(double x, double y)
     {
-        const auto theta2 = std::acos((std::pow(x, 2) + std::pow(y, 2) - std::pow(l2_, 2) - std::pow(l2_, 2)) / (2.0 * l1_ * l2_));
-        const auto theta1 = std::atan2(y,x) - std::atan2(l2_ * std::sin(theta2), l1_ + l2_ * std::cos(theta2));
+        const auto arg = std::clamp((x*x + y*y - l2_*l2_ - l1_*l1_) / (2.0 * l1_ * l2_), -1.0, 1.0);
+        const auto theta2 = std::acos( arg );
+        // std::cout << "arg: " << arg << " acos(arg): " << theta2 << std::endl;
+        const auto theta1 = std::atan2(y,x) - std::atan2( l2_ * std::sin(theta2), l1_ + l2_ * std::cos(theta2) );
         return { theta1, theta2 };
     }
 
